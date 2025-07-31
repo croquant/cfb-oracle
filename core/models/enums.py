06ca504c22b3f05b619217190820aa1333e4f87c
Maxime Desmet Vanden Stock <maxime.desmet@breev.eu>
2025-07-31 16:29:54 +0200

    Add SeasonType enumeration for NCAA season types

--- core/models/enums.py
+++ core/models/enums.py
@@ -8,3 +8,14 @@ class DivisionClassification(models.TextChoices):
     FCS = "fcs", "FCS"
     II = "ii", "Division II"
     III = "iii", "Division III"
+
+
+class SeasonType(models.TextChoices):
+    """Enumeration for NCAA season types."""
+
+    REGULAR = "regular", "Regular"
+    POSTSEASON = "postseason", "Postseason"
+    BOTH = "both", "Both"
+    ALLSTAR = "allstar", "Allstar"
+    SPRING_REGULAR = "spring_regular", "Spring Regular"
+    SPRING_POSTSEASON = "spring_postseason", "Spring Postseason"
