c059b33c2da0880d872f0c3a53232fbf65bdf255
Maxime Desmet Vanden Stock <maximedesmet@yahoo.com>
2025-07-31 11:41:19 +0200

    Display team logos in admin

--- core/admin/team_admin.py
+++ core/admin/team_admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from unfold.admin import ModelAdmin, TabularInline
 
 from core.models.team import Team, TeamAlternativeName, TeamLogo
@@ -13,14 +14,28 @@ class TeamAlternativeNameTabularInline(TabularInline):
 
 class TeamLogoInline(TabularInline):
     model = TeamLogo
-    fields = ("url",)
+    fields = ("url", "preview")
+    readonly_fields = ("preview",)
     extra = 0
+
+    def preview(self, obj):
+        if obj.url:
+            return format_html(
+                '<img src="{}" class="h-8" />',
+                obj.url,
+            )
+        return format_html(
+            '<span class="material-symbols-outlined text-base-400">image</span>'
+        )
+
+    preview.short_description = "Logo"
 
 
 @admin.register(Team)
 class TeamAdmin(ModelAdmin):
     search_fields = ("school", "mascot")
     list_display = (
+        "logo_display",
         "school",
         "mascot",
         "abbreviation",
@@ -31,5 +46,14 @@ class TeamAdmin(ModelAdmin):
         "conference",
     )
     list_filter_sheet = False
+    readonly_fields = ("logo_display",)
     inlines = [TeamAlternativeNameTabularInline, TeamLogoInline]
+
+    def logo_display(self, obj):
+        logo = obj.logos.first()
+        if logo:
+            return format_html('<img src="{}" class="h-8" />', logo.url)
+        return format_html('<span class="material-symbols-outlined text-base-400">image</span>')
+
+    logo_display.short_description = "Logo"
 
