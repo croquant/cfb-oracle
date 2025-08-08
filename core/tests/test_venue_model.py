03914f77441bb3f24fb9691fefd4ff16581c2412
Maxime Desmet Vanden Stock <maximedesmet@yahoo.com>
2025-08-08 15:24:02 +0200

    test: add venue model tests

---
+++ core/tests/test_venue_model.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+
+from core.models.venue import Venue
+
+
+class VenueModelTests(TestCase):
+    def test_str_returns_name_and_location(self):
+        """``__str__`` returns '<name> (<city>, <state>)'."""
+        venue = Venue.objects.create(name="Memorial Stadium", city="Athens", state="GA")
+        self.assertEqual(str(venue), "Memorial Stadium (Athens, GA)")
+
+    def test_meta_options(self):
+        """Model meta options specify ordering and verbose names."""
+        self.assertEqual(Venue._meta.ordering, ["name"])
+        self.assertEqual(Venue._meta.verbose_name, "venue")
+        self.assertEqual(Venue._meta.verbose_name_plural, "venues")
